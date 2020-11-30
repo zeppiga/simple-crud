14f8194fb65c7eebf0155c019431e3733d67f29c
M Cepiga <michal.cepiga@medius.com>
2020-11-30 12:01:43 +0100

    logging middleware bug fix

--- simple-crud/Middleware/LoggingMiddleware.cs
+++ simple-crud/Middleware/LoggingMiddleware.cs
@@ -18,12 +18,14 @@ namespace simple_crud.Middleware
 
         public async Task InvokeAsync(HttpContext context)
         {
-            _logger.LogInformation($"Request \n Host: {context.Request.Host} \n Path: {context.Request.Path} \n QueryString: {context.Request.QueryString} \n Body: {await GetBody(context)}");
+            _logger.LogInformation($"Request Host: {context.Request.Host} Path: {context.Request.Path} QueryString: {context.Request.QueryString} Method: {context.Request.Method} \n Body: {await GetBody(context)}");
             await _next(context);
         }
 
         private static async Task<string> GetBody(HttpContext context)
         {
+            context.Request.EnableBuffering();
+
             // TODO consider RecyclableMemoryStream as there could be problems when requests are large enough for loh
             await using var ms = new MemoryStream();
             await context.Request.Body.CopyToAsync(ms);
@@ -31,6 +33,8 @@ namespace simple_crud.Middleware
             ms.Seek(0, SeekOrigin.Begin);
             using var reader = new StreamReader(ms);
             var result = await reader.ReadToEndAsync();
+            ms.Seek(0, SeekOrigin.Begin);
+            context.Request.Body.Position = 0;
 
             return result;
         }
